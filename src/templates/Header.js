808f48eb49647f8f6aff18dae942cd0771f1f99f
Niyuby Blanco <nblanco.nb17@gmail.com>
2021-03-15 18:47:43 -0430

    First commit Travis

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     const view = `
     <div class="Header-main">
     <div class="header-logo">
-    <a href="/"><img src="https://i.imgur.com/7utDxm8.png" alt="Rick and Morty"></a>
+    <a href="/curso-spa"><img src="https://i.imgur.com/7utDxm8.png" alt="Rick and Morty"></a>
        </div> 
        <div class="Header-nav">
        <a href="#/about/">
