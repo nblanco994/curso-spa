b1d15340d238b359122021d00290abbb4332cd0d
Niyuby Blanco <nblanco.nb17@gmail.com>
2021-03-15 16:52:10 -0430

    First commit Travis

---
+++ src/templates/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    const view = `
+    <div class="Header-main">
+    <div class="header-logo">
+    <a href="/"><img src="https://i.imgur.com/7utDxm8.png" alt="Rick and Morty"></a>
+       </div> 
+       <div class="Header-nav">
+       <a href="#/about/">
+       About
+        </a> 
+       </div> 
+    </div> 
+  `;
+    return view;
+};
+
+export default Header;
